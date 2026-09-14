39f48b1e4761e7291639156f3f1d126468b25c77
yamineinzali54-star <yamineinzali54@gmail.com>
2026-09-14 13:24:32 +0630

    Initial student grade program

---
+++ grade.py
@@ -0,0 +1,8 @@
+name = input("Enter student name: ")
+mark1 = float(input("Enter first subject mark: "))
+mark2 = float(input("Enter second subject mark: "))
+
+avg_mark = (mark1 + mark2) / 2
+
+print(f"Student Name: {name}")
+print(f"Average Mark: {avg_mark}")
